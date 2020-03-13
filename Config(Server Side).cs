bb91bab05e175bcd6fc7ebd9687ee4f05c4435f1
%7B196ff1a5-c642-4078-a700-d16564f8954b%7D <alperrtorun@gmail.com>
2020-03-13 21:21:43 +0300

    .

---
+++ Config(Server Side).cs
@@ -0,0 +1,55 @@
+﻿
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+
+            };
+
+
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+               
+                new Client
+                {
+                    ClientId = "G T S",
+                    ClientSecrets = { new Secret("VE SECRET DEGERI".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+                
+                  // Login işleminden sonra aktarılacak 
+                    RedirectUris = { "http://gts.lojistork.com/signin-oidc" },   // Oidc kütüphanesinden  karmaşık bir adres satırı üretiyor.
+
+                   // Logout işleminden sonra                               
+                    PostLogoutRedirectUris = { "http://gts.lojistork.com/Account/Login" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    },
+
+                    AllowOfflineAccess = true
+                }
+            };
+    }
+}
---
+++ Startup(Server Side).cs
@@ -0,0 +1,69 @@
+﻿
+using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer()      //BU BÖLÜM SİZİN STARTUP CLASS INIZIN AYARLARI OLACAKTIR;
+                .AddInMemoryIdentityResources(Config.Ids)  // CONFİG CLASS I NI  BURALARA CONFIGURE EDİYORUZ.
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users);  // TÜM BİLGİLER EŞLEŞİYOR İSE, LOGİN SAYFAMA YÖNLENDİRİYOR  BENİ VE BURDA BULUNANAN TEST USERLER ile GİRİŞ SAĞLIYORUM.
+                                                                                  //     SIZIN EKLEYECEĞİNİZ DATABASE İLE ALAKALI MUHTEMEL ŞEYLER OLABİLİR BURADA
+            builder.AddDeveloperSigningCredential();
+
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    options.ClientId = "ID";
+                    options.ClientSecret = "SECRET";
+                })
+                .AddOpenIdConnect("oidc", "Demo IdentityServer", options =>
+
+                {
+                    // BURADAKI VERİLER SİZİNLE AZURE ARASINDAKI BAGLANTI İÇİN GEREKEN BİLGİLERİNİZ
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+                    options.SaveTokens = true;
+
+                    options.Authority = "https://demo.identityserver.io/";     
+                    options.ClientId = "native.code";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                  
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
